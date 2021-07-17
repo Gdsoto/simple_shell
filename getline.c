e4a70c7fac4659ca14e08b8384c3eeb3013e4e89
giosermon <2172@holbertonschool.com>
2021-07-16 19:56:50 -0500

    update fun ction

--- getline.c
+++ getline.c
@@ -5,11 +5,40 @@
  */
 char *getLines()
 {
-	char *line = NULL;
-	size_t lineSize = 0;
+	char *buffer = NULL, *cp_buff = NULL;
+	int position = 0, c = 0;
 
-	getline(&line, &lineSize, stdin);
-	return (line);
+	buffer = _calloc(BUFSIZ, sizeof(char));
+	if (!buffer)
+	{
+		perror("./hsh");
+		exit(EXIT_FAILURE);
+	}
+	while ((c = _getchar()))
+	{
+
+		if (c == EOF)
+		{
+			buffer[position] = '\0';
+			cp_buff = _strdup(buffer);
+			free(buffer);
+			return (cp_buff);
+		}
+		if (c == '\n')
+		{
+			buffer[position] = '\n';
+			cp_buff = _strdup(buffer);
+			free(buffer);
+			return (cp_buff);
+		}
+		else
+		{
+			buffer[position] = c;
+		}
+		position++;
+	}
+	return (NULL);
+}
 }
 
 /**
